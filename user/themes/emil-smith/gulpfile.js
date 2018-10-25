48320468c365d241176408f9a8e5167da38a8123
Chewy Jetpack <chewyjetpack@gmail.com>
2018-10-25 14:19:32 +0100

    set up js gulpfile, babel etc

--- user/themes/emil-smith/gulpfile.js
+++ user/themes/emil-smith/gulpfile.js
@@ -5,35 +5,38 @@ var csscomb = require('gulp-csscomb');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
-
-// configure the paths
-var watch_dir = './scss/**/*';
-var src_dir = './scss/*';
-var dest_dir = './css-compiled';
-
-var paths = {
-    source: src_dir
-};
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
 
 gulp.task('watch', function() {
-  gulp.watch(watch_dir, ['build']);
+  gulp.watch(["./scss/**/*.scss", "./js/**/*.js"], ["scss", "js"]);
 });
 
-gulp.task('build', function() {
-  gulp.src(paths.source)
+gulp.task('scss', function () {
+  gulp
+    .src("./scss/**/*.scss")
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
-    )
+    .pipe(sass({
+        outputStyle: "compact",
+        precision: 10
+      }).on("error", sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(autoprefixer())       
-    .pipe(gulp.dest(dest_dir))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("./dist/css/"))
     .pipe(csscomb())
     .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(dest_dir));
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("./dist/css/"));
 });
+
+gulp.task('js', function () {
+  gulp.src(['./js/**/*.js', 'node_modules/babel-polyfill/dist/polyfill.js'])
+    .pipe(sourcemaps.init())
+    .pipe(babel({ presets: ['env'] }))
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/js/'));
+});
+
 
 gulp.task('default', ['build']);
