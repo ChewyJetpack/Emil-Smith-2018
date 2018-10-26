f4b717b5f6e0ab8dc03a5a0b8695635994c0fa8d
Chewy Jetpack <chewyjetpack@gmail.com>
2018-10-26 13:58:46 +0100

    smooth scroll

--- user/themes/emil-smith/js/main.js
+++ user/themes/emil-smith/js/main.js
@@ -1,6 +1,19 @@
-const galleries = document.querySelectorAll(".gallery");
+const galleries = document.querySelectorAll('.gallery');
 
 galleries.forEach(gallery => {
     const gallerySet = gallery.querySelector('a').getAttribute('class');
     const lightboxDescription = GLightbox({ selector: gallerySet });
+});
+
+const navItems = document.querySelectorAll('.smoothscroll');
+
+navItems.forEach(navItem => {
+    const elemId = navItem.getAttribute("href");
+    const elem = document.querySelector(elemId);
+    const topPos = elem.getBoundingClientRect().top - 69;
+    console.log(elem, topPos);
+    navItem.addEventListener("click", function(e) {
+        e.preventDefault();
+        window.scrollTo({ top: topPos, behavior: "smooth" });
+    });
 });
