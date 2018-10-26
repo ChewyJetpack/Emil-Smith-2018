22115361a1c2308d45553e4ae7ae61b6db42d216
Chewy Jetpack <chewyjetpack@gmail.com>
2018-10-26 06:51:25 +0100

    colour scheme, smoother anims on portfolio

--- cache/compiled/config/master-emil.local.php
+++ cache/compiled/config/master-emil.local.php
@@ -1,85 +1,85 @@
 <?php
 return [
     '@class' => 'Grav\\Common\\Config\\CompiledConfig',
-    'timestamp' => 1540455756,
-    'checksum' => '2dc65d7080800cc00c4ee3f235c7d91b',
+    'timestamp' => 1540492920,
+    'checksum' => 'a3c931998d8785a0917fb3cd9b976bc4',
     'files' => [
         'user/config' => [
             'media' => [
                 'file' => 'user/config/media.yaml',
-                'modified' => 1540370783
+                'modified' => 1540406120
             ],
             'security' => [
                 'file' => 'user/config/security.yaml',
-                'modified' => 1540370782
+                'modified' => 1540406120
             ],
             'site' => [
                 'file' => 'user/config/site.yaml',
-                'modified' => 1540385587
+                'modified' => 1540406120
             ],
             'streams' => [
                 'file' => 'user/config/streams.yaml',
-                'modified' => 1540370783
+                'modified' => 1540406120
             ],
             'system' => [
                 'file' => 'user/config/system.yaml',
-                'modified' => 1540455681
+                'modified' => 1540444986
             ]
         ],
         'system/config' => [
             'media' => [
                 'file' => 'system/config/media.yaml',
-                'modified' => 1539042373
+                'modified' => 1540406118
             ],
             'security' => [
                 'file' => 'system/config/security.yaml',
-                'modified' => 1539042373
+                'modified' => 1540406118
             ],
             'site' => [
                 'file' => 'system/config/site.yaml',
-                'modified' => 1539042373
+                'modified' => 1540406118
             ],
             'streams' => [
                 'file' => 'system/config/streams.yaml',
-                'modified' => 1539042373
+                'modified' => 1540406118
             ],
             'system' => [
                 'file' => 'system/config/system.yaml',
-                'modified' => 1540393617
+                'modified' => 1540406118
             ]
         ],
         'user/plugins' => [
             'plugins/admin' => [
                 'file' => 'user/plugins/admin/admin.yaml',
-                'modified' => 1539042373
+                'modified' => 1540406120
             ],
             'plugins/devtools' => [
                 'file' => 'user/plugins/devtools/devtools.yaml',
-                'modified' => 1540377885
+                'modified' => 1540406124
             ],
             'plugins/email' => [
                 'file' => 'user/plugins/email/email.yaml',
-                'modified' => 1539042373
+                'modified' => 1540406124
             ],
             'plugins/error' => [
                 'file' => 'user/plugins/error/error.yaml',
-                'modified' => 1539042373
+                'modified' => 1540406125
             ],
             'plugins/form' => [
                 'file' => 'user/plugins/form/form.yaml',
-                'modified' => 1539042373
+                'modified' => 1540406125
             ],
             'plugins/login' => [
                 'file' => 'user/plugins/login/login.yaml',
-                'modified' => 1539042373
+                'modified' => 1540406126
             ],
             'plugins/markdown-notices' => [
                 'file' => 'user/plugins/markdown-notices/markdown-notices.yaml',
-                'modified' => 1539042373
+                'modified' => 1540406128
             ],
             'plugins/problems' => [
                 'file' => 'user/plugins/problems/problems.yaml',
-                'modified' => 1539042373
+                'modified' => 1540406128
             ]
         ]
     ],
